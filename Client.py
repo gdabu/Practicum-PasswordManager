47f3dcea4ff85893500b2d64b5d2a324afc09f0b
Geoff Dabu <geoffdabu@gmail.com>
2016-02-29 13:17:44 -0800

    added ability to enable/disable 2fa

--- Client.py
+++ Client.py
@@ -25,6 +25,18 @@ def clientHandler():
                 commandData = json.dumps({"action" : "REGISTER", "username":userName, "password":userPassword});   
                 sock.sendall(commandData)
 
+            elif command == "2FA_ENABLE":
+                while True:
+                    enabled = raw_input('Enable 2FA [TRUE/FALSE/CANCEL]:')
+                    if enabled == "TRUE" or enabled == "FALSE":
+                        commandData = json.dumps({"action" : "2FA_ENABLE", "enabled" : enabled})
+                        sock.sendall(commandData)
+                        break
+                    elif enabled == "CANCEL":
+                        break
+                    else:
+                        print "Invalid Option"
+
             elif command == "LOGOUT":
                 commandData = json.dumps({"action" : "LOGOUT"});
                 sock.sendall(commandData)
@@ -40,8 +52,6 @@ def clientHandler():
                 
                 # get data
                 data = sock.recv(4096)
-
-                print len(data)
 
                 if not data:
                     print 'Server Connection Terminated: Closing Socket'
@@ -68,7 +78,7 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
 try:
-    server_address = ('192.168.0.35', 8000)
+    server_address = ('192.168.0.28', 8000)
     print 'connecting to %s port %s' % server_address
     sock.connect(server_address)
     clientHandler()
--- Server.py
+++ Server.py
@@ -1,16 +1,21 @@
 from socket import *
 import thread
 import json
+import MySQLdb
 
-HOST = '192.168.0.35'# must be input parameter @TODO
+HOST = '192.168.0.28'# must be input parameter @TODO
 PORT = 8000 # must be input parameter @TODO
 
 testUser = "test"
 testPassword = "test"
 
-def handler(clientsock, addr):
+def handler(clientsock, addr, db):
 
-    loggedIn = False
+    cursor = db.cursor()
+    loggedIn = True
+
+    loggedInUser = "test"
+
     try:
         while 1:
 
@@ -28,46 +33,124 @@ def handler(clientsock, addr):
             # 
             commandData = json.loads(data.rstrip());
             command = commandData['action']
-            
-            if command == "REGISTER":
-                clientsock.sendall(data)
 
+            # 
+            # Register
+            # 
+            if command == "REGISTER":
+
+
+                try: 
+                    
+                    # insert registered username and password
+                    cursor.execute("insert into users (username, password) values ('" + commandData['username'] + "', '" + commandData['password'] + "')")
+                    db.commit()
+                    clientsock.sendall("Registration Successfull")
+
+                # catch exception for when username is already taken
+                except MySQLdb.IntegrityError, e:
+                    
+                    # 1062 is the error number for inserting and already existing primary key value.
+                    if not e[0] == 1062:
+                        clientsock.sendall("Registration Unsuccessfull")
+                        raise
+                    else:
+                        clientsock.sendall("Registration Unsuccessfull: Username already taken")
+                        print "Username already taken"
+                        db.rollback()
+                continue
+
+            # 
+            # Register
+            # 
             elif command == "LOGIN":
-                
-                if commandData['username'] == "test" and commandData['password'] == "test":
-                    loggedIn = True
-                    clientsock.sendall("LOGIN Successfull")
-                else:
+
+                cursor.execute("select * from users where username = '" + commandData['username'] + "'" )
+
+                user = cursor.fetchall()
+
+                if len(user) == 0:
+                    print "User not found"
                     clientsock.sendall("LOGIN Unsuccessfull")
+                else:
+                    print "user found"
+                    for row in user:
+                        if row[1] == commandData['password']:
+                            print "Login Successfull"
+                            loggedIn = True
+                            loggedInUser = row[0]
+                            clientsock.sendall("LOGIN Successfull")
+                        else:
+                            print "Wrong Password"
+                            clientsock.sendall("LOGIN Unsuccessfull")
+                        break
+                continue
+
+            # 
+            # 2FA_ENABLE
+            # 
+            elif command == "2FA_ENABLE" and loggedIn == True:
+                
+                tfa_enabled = 0
+                
+                if commandData['enabled'] == "TRUE":
+                    tfa_enabled = 1
+
+                try:
+                    cursor.execute("update users set tfa_enabled=" + `tfa_enabled` + " where username='" +loggedInUser+ "'" )
+                    db.commit()
+                    if tfa_enabled 
+                    clientsock.sendall("2fa enabled/disabled")
+                except error, e:
+                    print e
+                    db.rollback()
+                    clientsock.sendall("2fa was not enabled/disabled")
+
+                continue
+
             
+            # 
+            # Login
+            # 
             elif data.rstrip() == "SYNC" and loggedIn == True:
                 clientsock.sendall(data)
+                continue
 
+            # 
+            # CRUD
+            # 
             elif data.rstrip() == "CRUD" and loggedIn == True:
                 clientsock.sendall(data)
+                continue
 
+            # 
+            # NOT LOGGED IN
+            # 
             elif (data.rstrip() == "SYNC" or data.rstrip() == "CRUD") and loggedIn == False:
                 print addr, "- Not Logged In"
                 clientsock.sendall(data)
+                continue
 
-
+            # 
+            # Logout
+            # 
             elif command == "LOGOUT" and loggedIn == True:
                 loggedIn = False
                 print addr, " - Logged Out"
                 break;
 
+            # 
+            # Invalid Command
+            # 
             else:
                 clientsock.sendall(data)
                 print "not a command"
+                continue
 
         clientsock.close()
 
-
-    except socket.error:
+    except error, e:
         print "Socket error"
-        clientsock.close()
-    except KeyboardInterrupt:
-        print "00000000"
     finally: 
         clientsock.close()
 
@@ -78,9 +161,10 @@ if __name__=='__main__':
     serversock.bind(ADDR)
     serversock.listen(5)
 
+    db = MySQLdb.connect(host="localhost", user="root", passwd="bastard11", db="pwd_manager")
 
     while 1:
         print '>> Server Listening on Port: ', PORT
         clientsock, addr = serversock.accept()
         print '>> Connected to: ', addr
-        thread.start_new_thread(handler, (clientsock, addr))
+        thread.start_new_thread(handler, (clientsock, addr, db))
