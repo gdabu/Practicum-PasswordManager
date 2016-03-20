4e93f4febd41a0ff0771c68e37462ea4bc4d1d78
Geoff Dabu <geoffdabu@gmail.com>
2016-03-20 01:28:41 -0700

    server cmd line

--- Server.py
+++ Server.py
@@ -383,8 +383,8 @@ def handler(clientsock, addr, db, logger):
 if __name__=='__main__':
 
     cmdParser = argparse.ArgumentParser(description="8505A1-CovertChannel Client")
-    cmdParser.add_argument('-i','--IP',IP='IP', help='HOST Ip', required=True)
-    cmdParser.add_argument('-i','--PORT',PORT='PORT', help='HOST Port', required=True)
+    cmdParser.add_argument('-i','--IP',dest='IP', help='HOST Ip', required=True)
+    cmdParser.add_argument('-i','--PORT',dest='PORT', help='HOST Port', required=True)
     args = cmdParser.parse_args();
 
 
