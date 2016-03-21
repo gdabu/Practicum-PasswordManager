e5353df0b67a34706b5deaa6ecdd30675ad768df
Geoff Dabu <geoffdabu@gmail.com>
2016-03-21 00:39:50 -0700

    gui sync working

--- login_kivy.py
+++ login_kivy.py
@@ -7,7 +7,7 @@ import socket
 from ClientConnection import ClientConnection
 from PasswordCrud import *
 
-HOST = '192.168.0.181'
+HOST = '192.168.0.197'
 # HOST = '142.232.169.184'
 PORT = 8000
 
@@ -15,7 +15,96 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import StringProperty, ListProperty, ObjectProperty, BooleanProperty
-from kivy.uix.button import Button
+from kivy.uix.button import Button, Label
+
+class SyncScreen(Screen):
+    loggedInUser = StringProperty('')
+    localPasswordList = ListProperty([])
+    remotePasswordList = ListProperty([])
+
+    def syncWithRemote(self):
+        print "sync with remote"
+        commandData = json.dumps({"action" : "SYNC", "subaction" : "PULL"})
+        recvJsonData = self.parent.clientConnection.send_receive(commandData)
+
+        self.parent.cursor.execute("delete from passwords where username='" + self.loggedInUser + "'" )
+        pushPasswordData = ast.literal_eval(recvJsonData['additional']['passwords'])
+
+        for password in pushPasswordData:
+            self.parent.cursor.execute("insert into passwords (username, account, password) values ('" + self.loggedInUser + "', '" + password['account'] + "', '" + password['password'] + "')" )
+
+        self.parent.db.commit()
+
+        self.loadPasswordList_UI()
+
+    def syncWithLocal(self):
+        print "sync with local"
+        commandData = json.dumps({"action" : "SYNC", "subaction" : "PUSH", "passwords" : str(self.localPasswordList)})
+        recvJsonData = self.parent.clientConnection.send_receive(commandData)
+
+        self.loadPasswordList_UI()
+
+    def onPasswordButtonClick(self, instance):
+        
+        for button in self.ids.local_password_list.children:
+            button.background_color = (1,1,1,1)
+        for button in self.ids.remote_password_list.children:
+            button.background_color = (1,1,1,1)
+
+        instance.background_color = (0.8, 0.8, 0.8, 1)
+
+        self.currentAccount_pw = str(instance.pw_account)
+        self.currentId_pw = str(instance.pw_id)
+        self.currentPassword_pw = str(instance.pw_password)
+        
+        self.ids.password_info.text = "Account: {0}\nPassword: {1}".format(self.currentAccount_pw, self.currentPassword_pw)
+        self.ids.password_location.text = "Location: {0}".format(instance.pw_location)
+
+    def readPasswords(self):
+        commandData = json.dumps({"action" : "CRUD", "subaction" : "READ"})
+        recvJsonData = self.parent.clientConnection.send_receive(commandData)
+        self.remotePasswordList = ast.literal_eval(recvJsonData['additional']['passwords'])
+
+        passwordList = PasswordRead(self.parent.db, self.loggedInUser)
+        self.localPasswordList = ast.literal_eval(passwordList)
+    
+    def loadPasswordList_UI(self):
+        self.readPasswords()
+        self.ids.remote_password_list.clear_widgets()
+        self.ids.local_password_list.clear_widgets()
+
+        self.ids.remote_password_list.add_widget(Label(text="Remote Password List"))
+        self.ids.local_password_list.add_widget(Label(text="Local Password List"))
+
+        for entry in self.localPasswordList:
+
+            passwordBtn = PasswordButton(text=entry['account'], background_color=(0.93,0.93,0.93,1))
+
+            passwordBtn.pw_username = entry['username']
+            passwordBtn.pw_account=entry['account']
+            passwordBtn.pw_password=entry['password']
+            passwordBtn.pw_id=entry['id']
+
+            passwordBtn.pw_location = "Local"
+
+            passwordBtn.bind(on_release=self.onPasswordButtonClick)
+
+            self.ids.local_password_list.add_widget(passwordBtn)
+
+        for entry in self.remotePasswordList:
+
+            passwordBtn = PasswordButton(text=entry['account'], background_color=(0.93,0.93,0.93,1))
+
+            passwordBtn.pw_username = entry['username']
+            passwordBtn.pw_account=entry['account']
+            passwordBtn.pw_password=entry['password']
+            passwordBtn.pw_id=entry['id']
+
+            passwordBtn.pw_location = "Remote"
+
+            passwordBtn.bind(on_release=self.onPasswordButtonClick)
+
+            self.ids.remote_password_list.add_widget(passwordBtn)
 
 class RegistrationScreen(Screen):
     def register(self, username, password):
@@ -286,8 +375,6 @@ class MainScreenOnline(Screen):
         self.parent.current = "login_screen"
         pass
 
-        
-
 class MainScreenOffline(Screen):
     loggedInUser = StringProperty('')
     
@@ -370,8 +457,6 @@ class MainScreenOffline(Screen):
         self.parent.current = "login_screen"
         pass
 
-
-
 class ScreenManagement(ScreenManager):
     clientConnection = None
     db = None
@@ -387,7 +472,6 @@ class ScreenManagement(ScreenManager):
 
         print self.db
         print self.cursor
-
 
 presentation = Builder.load_file("main.kv")
 
