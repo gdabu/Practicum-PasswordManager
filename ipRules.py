e8636f6182fd5b2fdffa2247c24815698a8bcbfe
Geoff Dabu <geoffdabu@gmail.com>
2016-03-21 21:30:08 -0700

    added iprules file

--- ipRules.py
+++ ipRules.py
@@ -7,5 +7,5 @@ import os
 def blockIp(ip, duration):
 	print "BLOCKING MOFO================>"
 	retvalue = os.system("iptables -I INPUT -s " + ip + " -j DROP")
-	retvalue2 = os.system("echo 'iptables -D INPUT -s 192.168.1.100 -j DROP' | at now + " + `duration` + " min")
+	retvalue2 = os.system("echo 'iptables -D INPUT -s " + ip + " -j DROP' | at now + " + `duration` + " min")
 	print retvalue, retvalue2
